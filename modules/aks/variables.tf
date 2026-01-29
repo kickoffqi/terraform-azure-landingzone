1542c883fbce43d373a91ba4f6ef9b57cc7e9172
Jacob Qi <qiyu0001@gmail.com>
2026-01-30 08:06:56 +1100

    feat: add default authorized IP ranges for API server access

--- modules/aks/variables.tf
+++ modules/aks/variables.tf
@@ -95,4 +95,5 @@ variable "local_account_disabled" {
 variable "authorized_ip_ranges" {
   type        = set(string)
   description = "允许访问 API Server 的 IP 地址范围列表（CIDR 格式）"
+  default     = ["0.0.0.0/0"]
 }
